f6e050f0aa45cdfe9d0292067fca079fb50d6d2f
Ekokobe Selamo <selamo.ekokobe@stud.uni-due.de>
2020-09-05 23:52:53 +0200

    added login activity, signup and main activity

--- app/src/main/java/com/palava/palavaapplication/MainActivity.java
+++ app/src/main/java/com/palava/palavaapplication/MainActivity.java
@@ -2,13 +2,33 @@ package com.palava.palavaapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button loginBt,signUpBtn;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginBt = (Button) findViewById(R.id.mainLoginButton);
+        signUpBtn = (Button) findViewById(R.id.mainSignUpButton);
+        loginBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
